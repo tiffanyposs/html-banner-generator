3829353e7c4bfa34bd3861072745ab7d08d68f6f
Tiffany Poss <tiffanyposs@.com>
2019-01-03 13:58:37 -0500

    setup folder structure and copy existing sample banner

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
+import fse from 'fs-extra';
 import cheerio from 'cheerio';
-
+import sizeOf from 'image-size';
 
 const clicktagPartOne = (width, height) => {
 	const meta = `<meta name="ad.size" content="width=${width},height=${height}">`;
@@ -12,9 +13,8 @@ const clicktagPartTwo = () => {
 	return `<a href="javascript:window.open(window.clickTag)"></a>`;
 }
 
-// read contents of the src/sample-banner folder
-const init = () => {
-
+// adds clicktag to html in sample banner
+const addClicktags = () => {
 	// read the contents of the sample-banner folder
 	const folderRoot = 'src/project/sample-banner';
 	const contents = fs.readdirSync(folderRoot);
@@ -29,33 +29,76 @@ const init = () => {
 	// get the clicktag html and add it to the temp html file
 	const clicktagOne = clicktagPartOne(width, height);
 	const clicktagTwo = clicktagPartTwo();
-
 	$('title').after(clicktagOne[1]);
 	$('title').after(clicktagOne[0]);
-
 	$('canvas').wrap(clicktagTwo);
 
-	// make a new folder for processed folder, remove any old content
+	return $;
+}
+
+
+// matches sizes of the sample banner images to the size of the banner images
+// returns the matching sizes
+const getFilesToReplace = () => {
+	const extraImageFolder = 'src/project/images';
+	const extraImageSubFolders = fs.readdirSync(extraImageFolder);
+	const sampleImageNames = fs.readdirSync(`${extraImageFolder}/${extraImageSubFolders[0]}`);
+	const sampleImageDimentions = sizeOf(`${extraImageFolder}/${extraImageSubFolders[0]}/${sampleImageNames[0]}`);
+
+	const bannerImageFolder = 'src/project/sample-banner/images';
+	const bannerImageNames = fs.readdirSync(bannerImageFolder);
+	return bannerImageNames.filter(image => {
+		const size = sizeOf(`${bannerImageFolder}/${image}`);
+		return sampleImageDimentions.width === size.width && sampleImageDimentions.height === size.height;
+	});
+}
+
+// setup the processed folder banner
+const setupProcessedBannerFolder = () => {
+
+	const bannerRoutes = [];
+
+	// create a new processed-banners folder or empty the existing one
 	const newFolderRoot = 'src/project/processed-banners';
 	if (!fs.existsSync(newFolderRoot)) {
 		fs.mkdirSync(newFolderRoot);
 	} else {
-		const files = fs.readdirSync(newFolderRoot);
-		for (let file of files) {
-			fs.unlinkSync(`${newFolderRoot}/${file}`);
+		fse.emptyDirSync(newFolderRoot);
+	}
+
+	const filesToReplace = getFilesToReplace();
+
+	// make folders with same names as the images folder
+	const imageFolder = 'src/project/images';
+	const imageFolderNames = fs.readdirSync(imageFolder);
+	for (let folderName of imageFolderNames) {
+		fs.mkdirSync(`${newFolderRoot}/${folderName}`);
+		const images = fs.readdirSync(`${imageFolder}/${folderName}`);
+		const bannersToCreate = Math.ceil(images.length / filesToReplace.length); // calculates the number of banners based on number of assets
+		for (let i = 0; i < bannersToCreate; i++) {
+			const newBannerPath = `${newFolderRoot}/${folderName}/banner-${i+1}`;
+			fs.mkdirSync(newBannerPath);
+			fse.copySync('src/project/sample-banner', newBannerPath);
+			bannerRoutes.push(newBannerPath);
 		}
 	}
 
+	return bannerRoutes;
+}
 
-	
+// read contents of the src/sample-banner folder
+const init = () => {
+
+	// returns html with clicktag
+	const htmlWithClicktag = addClicktags();
+
+	// make a new folder for processed folder, remove any old content
+	const bannerRoutes = setupProcessedBannerFolder();
+
 
 
 
 }
-
-
-
-// add clicktag to the banner
 
 
 init();
